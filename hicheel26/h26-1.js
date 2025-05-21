8371879218d9f31811a90f3cd55cf0f909db7c92
osgoo <oosgoo966@gmail.com>
2025-05-21 19:08:13 +0800

    .

---
+++ hicheel26/h26-1.js
@@ -0,0 +1,40 @@
+// function greet(name){
+//     console.log(name);
+// }
+
+// let a = 'Ochir';
+// greet(a);
+
+// function add(a,b){
+    
+//     return a + b;
+// }
+
+// console.log(add(9,8));
+
+// let b = Number(prompt("1r too oruul"));
+// let c = Number(prompt("2r too oruul"));
+// let d = Number(prompt("3r too oruul"));
+
+// function avg(a,b,c){
+//     return (a+b+c)/3;
+// }
+
+// console.log(avg(b,c,d))
+
+let onoo = prompt("onoo oruul");
+
+function pass(a){
+    if(a>=60 && a<=100){
+        return console.log("tentssen");
+    }
+    else if(a>=0 && a<=59){
+        return console.log("tentseegu");
+    }
+    else{
+        return console.log("0-100 onoo oruul");
+    }
+}
+
+pass(onoo);
+
